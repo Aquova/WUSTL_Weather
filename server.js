110a8bb985573f543343f0426b793a766539f7ed
Aquova <abrick912@gmail.com>
2016-12-15 10:27:45 -0600

    Add files via upload
    Project now tweets on regular basis (3 hours). The time in AM/PM is also now displayed with each tweet. (Partially to bypass duplicate tweet restrictions).

--- server.js
+++ server.js
@@ -15,22 +15,52 @@ var T = new Twit(config);
 function tweetWeather(){
 	request({url: url, json: true}, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
+			var date = new Date(body.currently.time * 1000);
+			var hours = date.getHours();
+			if (hours > 12) {
+				half = 'PM'
+				hours = hours - 12
+			}
+			else if (hours == 0) {
+				half = 'AM'
+				hours = hours + 12
+			}
+			else if (hours == 12) {
+				half = 'PM'
+			}			
+			else {
+				half = 'AM'
+			}	
+			var minutes = '0' + date.getMinutes();
+			var time = hours + ':' + minutes.substr(-2);
+			time = time.toString();
 			temp = Math.floor(body.currently.temperature)
 			temp = temp.toString()
 			summary = body.currently.summary
 			summary = summary.toString()
-			output = 'It is currently ' + temp + 'ºF and ' + summary + '. #WUSTLweather'
+			output = 'Hello, it is ' + time + ' ' + half + '. The weather is currently ' + 
+						temp + 'ºF and ' + summary + '. #WUSTLweather'
+			console.log(output)
 			T.post('statuses/update', {status: output}, function(err, data, response) {
+				if (err) {
+					console.log('Something went wrong.');
+					console.log(err);
+				}
+				else {
+					console.log('I just tweeted: ')
+					console.log(output);
+				}
 				
 			});
-			//console.log('I just tweeted: ')
-			//console.log(output)
+			
+		}
+		else {
+			console.log('Something is wrong.');
+			console.log(error);
 		}
 	});	
 };
-	
 
-setInterval(
-	tweetWeather(),
-	timeDelay
-);
+tweetWeather();	
+
+setInterval(tweetWeather,timeDelay);
