be1cd29c6e8ad4272d6c2dc2bdc3f30ce98c06f3
Aquova <abrick912@gmail.com>
2016-12-15 11:23:27 -0600

    Add files via upload
    Included comments

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+// This is a twitterbot that tweets the current weather conditions of
+// Washington University in St. Louis
+// Bot can be seen at https://twitter.com/WUSTL_Weather
+// Written by Austin Bricker, 2016
+
+// Uses both 'request' and 'twit' additional libraries
 var request = require('request');
 var timeDelay = 10800000; // 3 hours
 var url = 'https://api.darksky.net/forecast/API_KEY/LATITUDE,LONGITUDE';
@@ -12,9 +18,11 @@ var fs = require('fs'),
 
 var T = new Twit(config);
 
+// Main tweet function
 function tweetWeather(){
 	request({url: url, json: true}, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
+			// Get UNIX date from JSON body, convert
 			var date = new Date(body.currently.time * 1000);
 			var hours = date.getHours();
 			if (hours > 12) {
@@ -34,13 +42,16 @@ function tweetWeather(){
 			var minutes = '0' + date.getMinutes();
 			var time = hours + ':' + minutes.substr(-2);
 			time = time.toString();
+			// Get temp, remove decimal values
 			temp = Math.floor(body.currently.temperature)
 			temp = temp.toString()
+			// Get weather conditions summary
 			summary = body.currently.summary
 			summary = summary.toString()
+			// Combine parts into desired tweet
 			output = 'Hello, it is ' + time + ' ' + half + '. The weather is currently ' + 
 						temp + 'ºF and ' + summary + '. #WUSTLweather'
-			console.log(output)
+			// Tweet, report status		
 			T.post('statuses/update', {status: output}, function(err, data, response) {
 				if (err) {
 					console.log('Something went wrong.');
@@ -61,6 +72,8 @@ function tweetWeather(){
 	});	
 };
 
+// Initially call tweet function
 tweetWeather();	
 
+// Set on delay until next tweet.
 setInterval(tweetWeather,timeDelay);
